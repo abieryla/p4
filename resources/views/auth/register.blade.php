8819258f1e36a3232daaf0f8b557f30d5892eade
Allyson Bieryla <abieryla@cfa.harvard.edu>
2016-05-11 19:02:04 -0400

    added errors on the registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,11 +9,16 @@
 	<p>Already have an account? <a href='/login'>Login here.</a></p>
         <h1>Register for an account</h1>
 
+	@if(count($errors) > 0)
+		<ul><div class='errors'>
+			@foreach($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	@endif
+
 	<form method='POST' action='/register'>
             {{ csrf_field() }}
-	    <div class="errors">
-            {{$errors->first('paragraph') }} <br>	    
-	    </div>
             First Name: 
 	    <input type='text' name='first' size='25' value='{{ old('first') }}'><br>
 	    Last Name:
@@ -25,7 +30,7 @@
 	    Re-enter password again:
 	    <input type='password' name='password_confirmation' size='50' value='{{ old('password_confirmation') }}'><br>
 
-            <input type='submit' value='Enter' ><br>
+            <input type='submit' value='Enter' class='btn btn-primary'><br>
 	
 	@if(count($errors) > 0)
 		<div class="errors">
